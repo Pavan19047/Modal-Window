0137f842839bd1e415fecab78702a6ec163de465
Pavan19047 <pavandimpu1234@gmail.com>
2024-01-23 15:47:36 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'use strict';
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+console.log(btnsOpenModal)
+
+const modalFunctionOpen = () => {
+  console.log('Button clicked')
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const modalFunctionClose = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', modalFunctionOpen)
+}
+
+btnCloseModal.addEventListener('click', modalFunctionClose)
+overlay.addEventListener('click', modalFunctionClose)
+document.addEventListener('keydown', e => {
+  // console.log(e);
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    modalFunctionClose()
+  }
+})
